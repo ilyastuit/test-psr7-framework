57821a96e13de5bebc0995f07d2572312c99f243
Ilyas <ilyastui@gmail.com>
2020-06-28 23:53:04 +0500

    Added request response manipulations

--- public/index.php
+++ public/index.php
@@ -1,3 +1,37 @@
 <?php
 
-echo 'Hello';
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Diactoros\Response\SapiEmitter;
+use Zend\Diactoros\ServerRequestFactory;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+### Initialization
+
+$request = ServerRequestFactory::fromGlobals();
+
+### Action
+
+$path = $request->getUri()->getPath();
+$action = null;
+
+if ($path === '/') {
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse('Hello, ' . $name . '!');
+    };
+}
+
+if ($action) {
+    $response = $action($request);
+} else {
+    $response = new HtmlResponse('Undefined page', 404);
+}
+
+$response = $response->withHeader('X-Developer', 'Ilyas');
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
