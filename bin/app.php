7102b67419d7d8e027588aa8cbdb286593bd0168
Ilyas <ilyastui@gmail.com>
2020-06-29 10:44:23 +0500

    Added console app

---
+++ bin/app.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+
+use Symfony\Component\Console\Application;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/**
+ * @var \Psr\Container\ContainerInterface $container
+ */
+$container = require 'config/container.php';
+
+$cli = new Application('Application console');
